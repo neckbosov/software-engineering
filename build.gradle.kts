4fefca42b13c68cdcb03aa5f2df50bf9e8634f02
Nikita Bosov <neckbosov@gmail.com>
2021-10-27 20:57:07 +0300

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,8 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
 
 tasks.getByName<Test>("test") {
